e9ad04ab3ad9ef0c2a2b460e021fe4c58c7756ac
Wilson <wilsonchen.code@gmail.com>
2022-01-06 04:08:56 -0800

    Add menu button to mobile view

--- src/App.js
+++ src/App.js
@@ -5,32 +5,44 @@ import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
 import NavBar from './components/navbar';
 import Typography from '@mui/material/Typography';
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
+import DimensionsProvider from './DimensionsProvider';
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 function App() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container>
-        <NavBar/>
-        <Grid item xs={12}>
-          <Paper square 
-            sx={{
-              height: "400vh",
-              alignItems: "center",
-              textAlign: "center",
-              justifyContent: "center",
-              background: "black"
-            }}>
-            <Fade in={true} timeout={3000}>
-              <Paper sx={{padding: 5, background: "transparent", color: "white"}}>
-                <Typography variant="h4" component="div">
-                  Hello, I'm Wilson
-                </Typography>
-              </Paper>
-            </Fade>
-          </Paper>
+    <DimensionsProvider>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container>
+          <NavBar />
+          <Grid item xs={12}>
+            <Paper square
+              sx={{
+                height: "400vh",
+                alignItems: "center",
+                textAlign: "center",
+                justifyContent: "center",
+                background: "black"
+              }}>
+              <Fade in={true} timeout={6000}>
+                <Paper sx={{ padding: 5, background: "transparent", color: "white" }}>
+                  <ThemeProvider theme={theme}>
+                    <Typography variant="h2" component="div" sx={{ fontFamily: "Montserrat" }}>
+                      WILSON CHEN
+                    </Typography>
+                    <Typography variant="h3" component="div" sx={{ fontFamily: "Montserrat" }}>
+                      Stun Zeed
+                    </Typography>
+                  </ThemeProvider>
+                </Paper>
+              </Fade>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </DimensionsProvider>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,34 +1,110 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import Grow from '@mui/material/Grow';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { useDimensions } from '../DimensionsProvider';
+
+export const NavContext = createContext();
 
 export default function NavBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" sx={{bgcolor: "black", borderBottom: "solid gray 1px"}}>
-            <Toolbar sx={{justifyContent: "center"}}>
-                <Button color="inherit" 
-                    sx={{
-                        ':hover': {
-                            bgcolor: 'white',
-                            color: 'black',
-                    }}}>About</Button>
-                <Button color="inherit"
-                    sx={{
-                        ':hover': {
-                            bgcolor: 'white',
-                            color: 'black',
-                    }}}>Projects</Button>
-                <Button color="inherit"
-                    sx={{
-                        ':hover': {
-                            bgcolor: 'white',
-                            color: 'black',
-                    }}}>Contact</Button>
+  const navButtons = ["About", "Experience", "Projects", "Education", "Skills", "Contact"];
+  const { width } = useDimensions();
+  const wide = (width > 750 ? true : false);
+  const [open, setOpen] = React.useState(false);
+  const handleIconClick = () => {
+    setOpen(!open);
+  };
+  if (wide) {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ bgcolor: "black", borderBottom: "solid gray 1px" }}>
+          <Grow in={true} timeout={2000} unmountOnExit>
+            <Toolbar sx={{ justifyContent: "center" }}>
+              {navButtons.map((label, index) => (
+                <Button
+                  key={index}
+                  color="inherit"
+                  sx={{
+                    ':hover': {
+                      bgcolor: 'white',
+                      color: 'black',
+                    },
+                    margin: 2
+                  }}>
+                  {label}
+                </Button>
+              ))}
             </Toolbar>
+          </Grow>
         </AppBar>
-    </Box>
-  );
+      </Box>
+    );
+  } else {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed" elevation={0} sx={{ bgcolor: "transparent" }}>
+          <Toolbar position="fixed" sx={{ justifyContent: "left" }}>
+            <IconButton
+              color='inherit'
+              edge='start'
+              onClick={handleIconClick}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <NavContext.Provider value={{ open, handleIconClick }}>
+          <NavPopover />
+        </NavContext.Provider>
+      </Box>
+    );
+  }
+}
+
+function NavPopover() {
+  const { width } = useDimensions();
+  const wide = (width > 750 ? true : false);
+  if (!wide) {
+    return (
+      <NavContext.Consumer>
+        {({ open, handleIconClick }) => (
+          <Drawer
+            variant='temporary'
+            anchor='top'
+            open={open}
+          >
+            <Paper square
+              sx={{
+                height: "100vh",
+                alignItems: "center",
+                textAlign: "center",
+                justifyContent: "center",
+                background: "black",
+              }}>
+              <List>
+                <ListItem button={true} onClick={handleIconClick}>
+                  <ListItemText primary={'Close'} />
+                </ListItem>
+                <ListItem button={true}>
+                  <ListItemText primary={'About'} />
+                </ListItem>
+                <ListItem button={true}>
+                  <ListItemText primary={'Experience'} />
+                </ListItem>
+              </List>
+            </Paper>
+          </Drawer>
+        )}
+      </NavContext.Consumer>
+    );
+  }
 }
